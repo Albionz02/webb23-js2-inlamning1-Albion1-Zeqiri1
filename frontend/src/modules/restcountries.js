4a71222e5ae36ee5b4dd21c2bd9c5b05eb9fdf22
Albionz02 <albion.zeqiri@gritacademy.se>
2024-03-12 10:50:05 +0100

    .

---
+++ frontend/src/modules/restcountries.js
@@ -0,0 +1,14 @@
+export async function getCountries() {
+    const url = 'https://restcountries.com/v3.1/all'; // Ändrad till att hämta alla länder
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error('Network not response');
+        }
+        const countries = await response.json();
+        return countries;
+    } catch (error) {
+        console.error('Error fetching countries:', error);
+        return [];
+    }
+}
